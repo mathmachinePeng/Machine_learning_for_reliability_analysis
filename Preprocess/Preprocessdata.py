16100beb548eaa824aeb22c1f8a120353c3e5ac6
jp1989326 <jp1989326@gmail.com>
2015-08-13 11:18:13 +1000

    push

---
+++ Preprocess/Preprocessdata.py
@@ -0,0 +1,51 @@
+from pandas.core.frame import DataFrame
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+from sklearn.metrics.classification import accuracy_score, confusion_matrix
+import csv
+from sklearn.ensemble import RandomForestClassifier
+
+
+class standardprocess():
+    def __init__(self):
+        print "**************************************"
+    
+    def init_process(self,raw_data, ratio):
+        X=np.array(raw_data)
+        rows,dimens=np.shape(X)
+        new_rows= int(rows*ratio)
+        new_dimens= dimens-1       
+        return(rows, dimens, new_rows, new_dimens)
+        
+    def scaledivd(self, raw_data, ratio):
+        a, b, aa, bb = self.init_process(raw_data, ratio)
+        X=np.array(raw_data)
+        min_max_scaler=preprocessing.MinMaxScaler()
+        Xscale= min_max_scaler.fit_transform(X)
+
+        Train=Xscale[0:aa,0:bb]
+        Trainlabel=Xscale[0:aa,bb]
+        Test=Xscale[aa:a,0:bb]
+        Testlabel=Xscale[aa:a,bb]        
+        return(Train, Trainlabel, Test, Testlabel)
+    
+    def noscale(self, raw_data, ratio):
+        a, b, aa, bb = self.init_process(raw_data, ratio)
+        X=np.array(raw_data)
+        Train=X[0:aa,0:bb]
+        Trainlabel=X[0:aa,bb]
+        Test=X[aa:a,0:bb]
+        Testlabel=X[aa:a,bb]        
+        return(Train, Trainlabel, Test, Testlabel)
+    
+    def normalscale(self, raw_data, ratio):
+        X=np.array(raw_data)
+        rows, dimens, new_rows, new_dimens = self.init_process(raw_data, ratio)
+        X=preprocessing.scale(X)
+        Train=X[0:new_rows,0:new_dimens]
+        Trainlabel=X[0:new_rows,new_dimens]
+        Test=X[new_rows:rows,0:new_dimens]
+        Testlabel=X[new_rows:rows,new_dimens]        
+        return(Train, Trainlabel, Test, Testlabel)
