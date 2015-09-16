7d299bfbc36b3fc03d99742c2b5f112a7b5d8df0
peng <peng@peng-HP-Compaq-8100-Elite-CMT-PC>
2015-09-16 16:19:02 +1000

    test

--- Test_1/Test.py
+++ Test_1/Test.py
@@ -1,29 +1,158 @@
+import cPickle, gzip, numpy
+import theano
+import theano.tensor as T
+import pandas as pd
 
-import pymc as pm
-import matplotlib
-from matplotlib import pyplot as plt
+# load the dataset 
 
-p = pm.Uniform('p', lower = 0, upper = 1)
-p_true = 0.05
-N = 1500
-
-occurrences = pm.rbernoulli(p_true, N)
-
-print occurrences
-print occurrences.sum()
-
-print "What is the observed frequency in Group A? %.4f" % occurrences.mean()
-print "Does this equal the true frequency? %s" % (occurrences.mean() == p_true)
-
-obs = pm.Bernoulli("obs", p, value = occurrences, observed = True)
-
-mcmc = pm.MCMC([p, obs])
-mcmc.sample(18000, 1000)
+f = gzip.open('/home/peng/Downloads/tutorialtheano/mnist.pkl.gz', 'rb')
+train_set, valid_set, test_set = cPickle.load(f)
+f.close()
 
 
-plt.title("Posterior distribution of $p_A$, the true effectiveness of site A")
-plt.vlines(p_true, 0, 90, linestyle="--", label="true $p_A$ (unknown)")
-plt.hist(mcmc.trace("p")[:], bins=25, histtype="stepfilled", normed=True)
-plt.legend()
 
-plt.show()
+def shared_dataset(data_xy):
+    """ Function that loads the dataset into shared variables
+
+    The reason we store our dataset in shared variables is to allow
+    Theano to copy it into the GPU memory (when code is run on GPU).
+    Since copying data into the GPU is slow, copying a minibatch everytime
+    is needed (the default behaviour if the data is not in a shared
+    variable) would lead to a large decrease in performance.
+    """
+    data_x, data_y = data_xy
+    shared_x = theano.shared(numpy.asarray(data_x, dtype=theano.config.floatX))
+    shared_y = theano.shared(numpy.asarray(data_y, dtype=theano.config.floatX))
+    # When storing data on the GPU it has to be stored as floats
+    # therefore we will store the labels as ``floatX`` as well
+    # (``shared_y`` does exactly that). But during our computations
+    # we need them as ints (we use labels as index, and if they are
+    # floats it doesn't make sense) therefore instead of returning
+    # ``shared_y`` we will have to cast it to int. This little hack
+    # lets us get around this issue
+    return shared_x, T.cast(shared_y, 'int32')
+
+test_set_x, test_set_y = shared_dataset(test_set)
+valid_set_x, valid_set_y = shared_dataset(valid_set)
+train_set_x, train_set_y = shared_dataset(train_set)
+
+batch_size = 500    # size of the minibatch
+
+# accessing the third minibatch of the training set
+
+data  = train_set_x[2 * batch_size: 3 * batch_size]
+label = train_set_y[2 * batch_size: 3 * batch_size]
+
+""" learning a Classifer
+1. Zero-One Loss
+
+zero_one_loss = T.sum(T.neq(T.argmax(p_y_given_x), y))
+
+2. Negative Log-Likelihood Loss
+
+# note on syntax: T.arange(y.shape[0]) is a vector of integers [0,1,2,...,len(y)].
+# Indexing a matrix M by the two vectors [0,1,...,K], [a,b,...,k] returns the
+# elements M[0,a], M[1,b], ..., M[K,k] as a vector.  Here, we use this
+# syntax to retrieve the log-probability of the correct labels, y.
+
+NLL = -T.sum(T.log(p_y_given_x)[T.arange(y.shape[0], y])
+
+3. Stochastic descent
+# GRADIENT DESCENT
+
+while True:
+    loss = f(params)
+    d_loss_wrt_params = ... # compute gradient
+    params -= learning_rate * d_loss_wrt_params
+    if <stopping condition is met>:
+        return params
+# STOCHASTIC GRADIENT DESCENT
+
+
+
+for (x_i,y_i) in training_set:
+                            # imagine an infinite generator
+                            # that may repeat examples (if there is only a finite training set)
+    loss = f(params, x_i, y_i)
+    d_loss_wrt_params = ... # compute gradient
+    params -= learning_rate * d_loss_wrt_params
+    if <stopping condition is met>:
+        return params
+
+# Minibatch Stochastic gradient decent
+        
+for (x_batch,y_batch) in train_batches:
+                           # imagine an infinite generator
+                           # that may repeat examples
+   loss = f(params, x_batch, y_batch)
+   d_loss_wrt_params = ... # compute gradient using theano
+   params -= learning_rate * d_loss_wrt_params
+   if <stopping condition is met>:
+       return params       
+        
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Now regularization
+# symbolic Theano variable that represents the L1 regularization term
+L1 = T.sum(abs(param))
+
+# symbolic Theano variable that represents the squared L2 term
+L2_sqr = T.sum(param**2)
+
+# the loss
+loss = Nll + lambda_1*L1 + lambda_2*L2
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Early stopping validation set
+
+# early-stopping parameters
+patience = 5000  # look as this many examples regardless
+patience_increase = 2     # wait this much longer when a new best is
+                              # found
+improvement_threshold = 0.995  # a relative improvement of this much is
+                               # considered significant
+validation_frequency = min(n_train_batches, patience/2)
+                              # go through this many
+                              # minibatches before checking the network
+                              # on the validation set; in this case we
+                              # check every epoch
+
+best_params = None
+best_validation_loss = numpy.inf
+test_score = 0.
+start_time = time.clock()
+
+done_looping = False
+epoch = 0
+while (epoch < n_epochs) and (not done_looping):
+    # Report "1" for first epoch, "n_epochs" for last epoch
+    epoch = epoch + 1
+    for minibatch_index in xrange(n_train_batches):
+
+        d_loss_wrt_params = ... # compute gradient
+        params -= learning_rate * d_loss_wrt_params # gradient descent
+
+        # iteration number. We want it to start at 0.
+        iter = (epoch - 1) * n_train_batches + minibatch_index
+        # note that if we do `iter % validation_frequency` it will be
+        # true for iter = 0 which we do not want. We want it true for
+        # iter = validation_frequency - 1.
+        if (iter + 1) % validation_frequency == 0:
+
+            this_validation_loss = ... # compute zero-one loss on validation set
+
+            if this_validation_loss < best_validation_loss:
+
+                # improve patience if loss improvement is good enough
+                if this_validation_loss < best_validation_loss * improvement_threshold:
+
+                    patience = max(patience, iter * patience_increase)
+                best_params = copy.deepcopy(params)
+                best_validation_loss = this_validation_loss
+
+        if patience <= iter:
+            done_looping = True
+            break
+
+# POSTCONDITION:
+# best_params refers to the best out-of-sample parameters observed during the optimization
+
