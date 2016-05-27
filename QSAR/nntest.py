3f6f5139d183358b86eb24a8e66b9c2b48cb3a56
Peng Jiang <jp1989326@gmail.com>
2016-05-27 10:56:53 +1000

    keras

--- QSAR/nntest.py
+++ QSAR/nntest.py
@@ -1,34 +1,41 @@
 import os,pickle,sys,numpy,copy,scipy,scipy.io
-
+import numpy as np
 # --------------------------------------------
 # Parameters
 # --------------------------------------------
 #split = int(sys.argv[1]) # test split for cross-validation (between 0 and 5)
-split = 5
+splits = np.arange(0,5)
 # --------------------------------------------
 # Load data and models
 # --------------------------------------------
 #if not os.path.exists('qm7.mat'): os.system('wget http://www.quantum-machine.org/data/qm7.mat')
 dataset = scipy.io.loadmat('/home/peng/Documents/Project_C/QSAR_nlp/qm7.mat')
-nn = pickle.load(open('nn-%d.pkl'%split,'r'))
+acc_mae = 0.0
 
-print('results after %d iterations'%nn.nbiter)
+for split in splits:
+	print 
 
-Ptrain = dataset['P'][range(0,split)+range(split+1,5)].flatten()
-#Ptest  = dataset['P'][split]
-Ptest  = dataset['P'][split-1]
-for P,name in zip([Ptrain,Ptest],['training','test']):
-	# --------------------------------------------
-	# Extract test data
-	# --------------------------------------------
-	X = dataset['X'][P]
-	T = dataset['T'][0,P]
-
-	# --------------------------------------------
-	# Test the neural network
-	# --------------------------------------------
-	print('\n%s set:'%name)
-	Y = numpy.array([nn.forward(X) for _ in range(10)]).mean(axis=0)
-	print('MAE:  %5.2f kcal/mol'%numpy.abs(Y-T).mean(axis=0))
-	print('RMSE: %5.2f kcal/mol'%numpy.square(Y-T).mean(axis=0)**.5)
-
+	nn = pickle.load(open('nn-%d.pkl'%split,'r'))
+	
+	print('results after %d iterations'%nn.nbiter)
+	
+	Ptrain = dataset['P'][range(0,split)+range(split+1,5)].flatten()
+	#Ptest  = dataset['P'][split]
+	Ptest  = dataset['P'][split-1]
+	for P,name in zip([Ptest],['test']):
+		# --------------------------------------------
+		# Extract test data
+		# --------------------------------------------
+		X = dataset['X'][P]
+		T = dataset['T'][0,P]
+	
+		# --------------------------------------------
+		# Test the neural network
+		# --------------------------------------------
+		print('\n%s set:'%name)
+		Y = numpy.array([nn.forward(X) for _ in range(10)]).mean(axis=0)
+		print('MAE:  %5.2f kcal/mol'%numpy.abs(Y-T).mean(axis=0))
+		acc_mae = acc_mae + numpy.abs(Y-T).mean(axis=0)
+		print('RMSE: %5.2f kcal/mol'%numpy.square(Y-T).mean(axis=0)**.5)
+		
+print ' the average_mae is ', acc_mae/5    
